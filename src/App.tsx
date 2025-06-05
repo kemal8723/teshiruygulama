59075c5d229f5d052ebab4e124fd4dcee0e37f95
kemal8723 <kemal.gulcan@hotmail.com.tr>
2025-06-05 17:56:02 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,19 +24,19 @@ function App(): React.ReactNode {
               <Route path="/store\" element={<StoreViewPage />} />
             )}
             {userRole === UserRole.STORE && !selectedStoreId && (
-               <Route path="/store" element={<Navigate to="/\" replace />} />
+               <Route path="/store\" element={<Navigate to="/\" replace />} />
             )}
 
             {userRole === UserRole.MANAGER && selectedManagerPersona && (
-              <Route path="/manager" element={<ManagerViewPage />} />
+              <Route path="/manager\" element={<ManagerViewPage />} />
             )}
              {userRole === UserRole.MANAGER && !selectedManagerPersona && (
                <Route path="/manager\" element={<Navigate to="/\" replace />} />
             )}
 
             <Route path="/store" element={<Navigate to="/\" replace />} />
-            <Route path="/manager" element={<Navigate to="/\" replace />} />
-            <Route path="*" element={<Navigate to="/\" replace />} />
+            <Route path="/manager\" element={<Navigate to="/\" replace />} />
+            <Route path="*\" element={<Navigate to="/\" replace />} />
           </Routes>
         </main>
         <footer className="bg-gradient-to-r from-teal-600 to-cyan-600 text-white text-center p-4 text-sm shadow-inner">
